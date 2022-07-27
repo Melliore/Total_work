bc20029b4b78e3ae16c246995c035d9152b9f469
Melliore <106551687+Melliore@users.noreply.github.com>
2022-07-27 18:01:01 +0500

    add c# file
    Добавлен файл решения задачи

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+
+
+
+
+
+  string [] mass = {"this", "is", "gonna", "be", "fun", "!"};
+
+string [] mass2 = new string [1];
+
+for (int i = 0; i < mass.Length; i++)
+{
+    for (int j = 0; j < mass2.Length; j++)
+    {
+        if  (mass[i].Length <= 3)
+        {
+            mass2[j] = mass[i];
+             Console.Write(mass2[j]+" ");
+        }
+    }
+}
+ 
+
+
+
+
+
+
+
