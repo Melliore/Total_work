705d666c5a1fdb7ef1534a851c9fdd1104354134
Melliore <106551687+Melliore@users.noreply.github.com>
2022-07-28 00:20:32 +0500

    Последняя версия контрольной работы

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+string[] mass = { "this", "is", "gonna", "be", "fun", "!", "-1" };
+
+string[] mass2 = {""};
+
+Console.WriteLine("Строки с числом символов 3 и менее: ");
+
+for (int i = 0; i < mass.Length; i++)
+{
+    for (int j = 0; j < mass2.Length; j++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            mass2[j] = mass[i];
+            Console.Write(mass2[j] + "  ");
+        }
+
+    }
+
+}
+
+
+
+
+
